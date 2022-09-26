bceb047e9b74f95b7271c9f05535a8f2d3b35490
ProgramacionDH <marcelocanay@hotmail.com>
2022-09-26 15:48:08 -0300

    m

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname,'/views/home.html'));
 });
 
-app.use(express.static(path.join(__dirname, './public')))
+app.use(express.static(path.join(__dirname,'/public')))
 ,
 
 app.listen(3000, () => {
