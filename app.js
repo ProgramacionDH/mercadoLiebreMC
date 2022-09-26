1c8665beb9099983f4860d3fde44cf47c724c87d
ProgramacionDH <marcelocanay@hotmail.com>
2022-09-26 15:39:39 -0300

    m

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '/views/home.html'));
+  res.sendFile(path.join(__dirname,'/views/home.html'));
 });
 
 app.use(express.static(path.join(__dirname, './public')))
