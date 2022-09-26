24fca62df852d7e584f6b0b8a728494a237e19dc
ProgramacionDH <marcelocanay@hotmail.com>
2022-09-26 14:49:20 -0300

    rutas

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ const path = require('path');
 
 const app = express();
 
-app.get('/mensaje', function (req, res) {
-res.sendFile(path.join(__dirname, './views/home.html'));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/views/home.html'));
 });
 
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, './public')))
+,
 
 app.listen(3000, () => {
   console.log("Servidor corriendo");
-}); 
-
-app.use(express.static(public));
+})
