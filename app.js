f8285bc6a6f0f86848962d279680c25fc5edc72b
ProgramacionDH <marcelocanay@hotmail.com>
2022-10-04 23:15:28 -0300

    links

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ const path = require('path');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname,'./views/home.html'));
+  res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
 
-app.use('/public', express.static(path.join(__dirname, 'public')))
-,
+app.get('/register', function (req, res) {
+  res.sendFile(path.resolve(__dirname, './views/register.html'));
+});
 
-app.listen(3000, () => {
+app.get('/login', function (req, res) {
+  res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
+
+app.use(express.static(path.resolve(__dirname, './public')));
+
+app.listen(process.env.PORT || 3000, function() {
   console.log("Servidor corriendo");
-})
+});
