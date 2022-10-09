d6075c5f68f85ab7c3c3d5c97db2ff47d268ff06
ProgramacionDH <marcelocanay@hotmail.com>
2022-10-09 20:59:47 -0300

    correciones

--- app.js
+++ app.js
@@ -4,18 +4,18 @@ const path = require('path');
 const app = express();
 
 app.get('/', (_req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'));
+    res.sendFile(path.join(__dirname, './views/home.html'));
   });
 
-app.get('/register', (_req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/register.html'));
+app.get('register', (_req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'));
   });
 
-app.get('/login', (_req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/login.html'));
+app.get('login', (_req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'));
   });
 
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.join(__dirname, './public')));
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Servidor corriendo");
