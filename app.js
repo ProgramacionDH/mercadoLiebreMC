bed5d7886d2d79f31881e0be6d1366bbd6aeb96e
ProgramacionDH <marcelocanay@hotmail.com>
2022-10-18 12:45:00 -0300

    links reparados

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'));
   });
 
-app.get('register', (req, res) => {
+app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, './views/register.html')); 
   });
 
-app.get('login', (req, res) => {
+app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, './views/login.html'));
   });
 
