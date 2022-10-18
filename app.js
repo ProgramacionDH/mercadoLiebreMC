805f0dedb305e75aa23d62071f2e35c30fd117f8
ProgramacionDH <marcelocanay@hotmail.com>
2022-10-18 10:41:00 -0300

    arreglando links

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
+const {log} = require('console');
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-app.get('/', (_req, res) => {
+app.use(express.static(path.join(__dirname, './public')));
+
+
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'));
   });
 
-app.get('register', (_req, res) => {
+app.get('register', (req, res) => {
     res.sendFile(path.join(__dirname, './views/register.html'));
   });
 
-app.get('login', (_req, res) => {
+app.get('login', (req, res) => {
     res.sendFile(path.join(__dirname, './views/login.html'));
   });
 
-app.use(express.static(path.join(__dirname, './public')));
+
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Servidor corriendo");
