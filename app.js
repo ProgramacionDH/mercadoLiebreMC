a46ea54dabd08461fd85675243699e342e823359
ProgramacionDH <marcelocanay@hotmail.com>
2022-10-18 12:36:13 -0300

    links

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('public')); 
-
+app.use(express.static(path.resolve(__dirname, './public')));
 
 
 app.get('/', (req, res) => {
